42717c29d12e57a94349dfef4c15fbf35b2a5f9d
yangchen <chen.yang1@thoughtworks.com>
2020-07-21 18:14:20 +0800

    add fragment to main activity

--- app/src/main/java/com/example/fragment/MainActivity.java
+++ app/src/main/java/com/example/fragment/MainActivity.java
@@ -1,14 +1,42 @@
 package com.example.fragment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FragmentManager fragmentManager;
+    private AndroidFragment androidFragment;
+    private JavaFragment javaFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragmentManager = getSupportFragmentManager();
+
+        androidFragment = new AndroidFragment();
+        javaFragment = new JavaFragment();
+
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.container,androidFragment);
+        fragmentTransaction.commit();
+    }
+
+    public void setAndroid(View view) {
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.container,androidFragment);
+        fragmentTransaction.commit();
+    }
+
+    public void setJava(View view) {
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.container,javaFragment);
+        fragmentTransaction.commit();
     }
 }
